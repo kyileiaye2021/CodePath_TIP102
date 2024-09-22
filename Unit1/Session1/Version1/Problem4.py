24f5672530f0cc2242e4289ac14ff840bcc4513b
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-09-22 01:08:11 +0000

    Solved problem4

--- Unit1/Session1/Version1/Problem4.py
+++ Unit1/Session1/Version1/Problem4.py
@@ -4,13 +4,16 @@
 # problem 4
 
 def non_decreasing(nums):
+  replaced = False
   for i in range(1, len(nums)):
-    if nums[i] < nums[i-1]:
+    if nums[i] < nums[i-1] and replaced:
       return False
+    elif nums[i] < nums[i-1]:
+      replaced = True
 
   return True
 
-nums = [1, 2, 3]
+nums = [4, 2, 3]
 print(non_decreasing(nums))
 
 nums = [4, 2, 1]
